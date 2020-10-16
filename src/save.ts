d2c97b8fd7702dac5da51f297d02c8920b38f313
Geoff Wagstaff <geoff@gosquared.com>
2020-10-16 11:09:32 +0000

    sequence token exception

--- src/save.ts
+++ src/save.ts
@@ -31,6 +31,15 @@ export function save(
   return cw.putLogEvents(params).promise();
 }
 
+/**
+ * can be called without token, but this requires an extra round trip to
+ * get from cwlogs.
+ * @param cw
+ * @param group
+ * @param stream
+ * @param messages
+ * @param token
+ */
 export async function retry(cw: CloudWatchLogs, group: string, stream: string, messages: Message[], token?: string): Promise<string | undefined> {
   let attempts = 0;
   let err: AWSError | undefined;
@@ -42,13 +51,27 @@ export async function retry(cw: CloudWatchLogs, group: string, stream: string, m
     err = e;
   }
 
+  /**
+   * From cw logs docs:
+   * "You must include the sequence token obtained from the response of
+   * the previous call. An upload in a newly created log stream does not
+   * require a sequence token. You can also get the sequence token in the
+   * expectedSequenceToken field from InvalidSequenceTokenException. If
+   * you call PutLogEvents twice within a narrow time period using the same
+   * value for sequenceToken, both calls might be successful or one might
+   * be rejected."
+   * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatchLogs.html#putLogEvents-property
+   */
   while (err) {
     if (attempts >= 5) {
       const err2 = new RetryError('retry limit reached');
       err2.e = err;
       throw err2;
     }
-    const match = err.message.match(/sequenceToken: (.+)/);
+    // DataAlreadyAcceptedException
+    let match = err.message.match(/sequenceToken: (.+)/);
+    // InvalidSequenceTokenException
+    if (!match) match = err.message.match(/sequenceToken is: (.+)/);
     if (!match) throw err;
     token = match[1];
     try {
